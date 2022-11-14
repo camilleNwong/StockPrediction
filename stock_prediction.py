35842f27a4c54657f5750d200c213fd8c7da9162
CamilleWong <camillew077@gmail.com>
2022-11-13 18:42:57 -0600

    first commit

---
+++ stock_prediction.py
@@ -0,0 +1,166 @@
+"""
+Stock market prediction using Markov chains.
+
+For each function, replace the return statement with your code.  Add
+whatever helper functions you deem necessary.
+"""
+
+import comp140_module3 as stocks
+
+
+### Model
+
+def markov_chain(data, order):
+    """
+    Create a Markov chain with the given order from the given data.
+
+    inputs:
+        - data: a list of ints or floats representing previously collected data
+        - order: an integer repesenting the desired order of the markov chain
+
+    returns: a dictionary that represents the Markov chain
+    """
+    return {}
+# ex:[0, 1, 2, 1, 2, 3, 0]
+# days: [0,1]
+for indx in range(len(data)):
+    days= tuple([indx:indx+order])
+    next=data[index+order]
+    #create helper method to keep track of how many times the output occurs after this pair
+    frequency(data, days, next)
+    
+#when checking, should we be splitting it into tuples???
+def frequency(data, days, next, order):
+    #will start at the beginning element of the data set
+    indx=0;
+    day=0;
+    while indx <= len(data):
+        #will go through each element in the given days
+        while day <= len(days):
+            while day
+            if day == indx:
+                continue
+            #if the day doesnt match index, 
+            #then break out of code and break out of helper
+          
+            
+                
+             
+                
+                
+    
+
+### Predict
+
+def predict(model, last, num):
+    """
+    Predict the next num values given the model and the last values.
+
+    inputs:
+        - model: a dictionary representing a Markov chain
+        - last: a list (with length of the order of the Markov chain)
+                representing the previous states
+        - num: an integer representing the number of desired future states
+
+    returns: a list of integers that are the next num states
+    """
+    return []
+
+
+### Error
+
+def mse(result, expected):
+    """
+    Calculate the mean squared error between two data sets.
+
+    The length of the inputs, result and expected, must be the same.
+
+    inputs:
+        - result: a list of integers or floats representing the actual output
+        - expected: a list of integers or floats representing the predicted output
+
+    returns: a float that is the mean squared error between the two data sets
+    """
+    return 0.0
+
+
+### Experiment
+
+def run_experiment(train, order, test, future, actual, trials):
+    """
+    Run an experiment to predict the future of the test
+    data given the training data.
+
+    inputs:
+        - train: a list of integers representing past stock price data
+        - order: an integer representing the order of the markov chain
+                 that will be used
+        - test: a list of integers of length "order" representing past
+                stock price data (different time period than "train")
+        - future: an integer representing the number of future days to
+                  predict
+        - actual: a list representing the actual results for the next
+                  "future" days
+        - trials: an integer representing the number of trials to run
+
+    returns: a float that is the mean squared error over the number of trials
+    """
+    return 0.0
+
+
+### Application
+
+def run():
+    """
+    Run application.
+
+    You do not need to modify any code in this function.  You should
+    feel free to look it over and understand it, though.
+    """
+    # Get the supported stock symbols
+    symbols = stocks.get_supported_symbols()
+
+    # Get stock data and process it
+
+    # Training data
+    changes = {}
+    bins = {}
+    for symbol in symbols:
+        prices = stocks.get_historical_prices(symbol)
+        changes[symbol] = stocks.compute_daily_change(prices)
+        bins[symbol] = stocks.bin_daily_changes(changes[symbol])
+
+    # Test data
+    testchanges = {}
+    testbins = {}
+    for symbol in symbols:
+        testprices = stocks.get_test_prices(symbol)
+        testchanges[symbol] = stocks.compute_daily_change(testprices)
+        testbins[symbol] = stocks.bin_daily_changes(testchanges[symbol])
+
+    # Display data
+    #   Comment these 2 lines out if you don't want to see the plots
+    stocks.plot_daily_change(changes)
+    stocks.plot_bin_histogram(bins)
+
+    # Run experiments
+    orders = [1, 3, 5, 7, 9]
+    ntrials = 500
+    days = 5
+
+    for symbol in symbols:
+        print(symbol)
+        print("====")
+        print("Actual:", testbins[symbol][-days:])
+        for order in orders:
+            error = run_experiment(bins[symbol], order,
+                                   testbins[symbol][-order-days:-days], days,
+                                   testbins[symbol][-days:], ntrials)
+            print("Order", order, ":", error)
+        print()
+
+# You might want to comment out the call to run while you are
+# developing your code.  Uncomment it when you are ready to run your
+# code on the provided data.
+
+# run()
